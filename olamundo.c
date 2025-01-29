a84313f60709463fc34f32c7f96ee6c2bb561c29
Pedro Magno <pedrofontenelerocha@outlook.com>
2025-01-29 12:52:24 +0000

    Pending changes exported from your codespace

--- olamundo.c
+++ olamundo.c
@@ -2,7 +2,10 @@
 
 int main (){
 
-    printf("ola, mundo \n");
+    char pais[15];
+
+    printf("Qual o nome do país a ser atribuido para a carta? \n");
+    scanf("%c", &pais);
 
     return 0;
 }
---
+++ super trunfo1/trunfo1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (){
+
+    char pais[15];
+    int codigo; // do 1 ao 4
+
+    printf("Qual o nome do país a ser atribuido para a carta? \n");
+    scanf("%s", &pais [10]);
+
+    printf("Registre o codigo dessa carta: \n"); 
+    scanf("%i", &codigo);
+
+    printf("Seu codigo é: %i\n", codigo);
+
+    printf("Seu codigo é: %i\n", codigo);
+    printf("País: %s\n", pais);
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Pontos turistícos: %d\n", Pturistico);
+    printf("Area: %f\n km²", area);
+    printf("PIB: %f\n", PIB);
+    printf("PIB per capita: %f\n", PIBpercapita);
+    printf("Densidade populacional: %d\n", densidadepopulacional);
+
+
+    return 0;
+
+}
