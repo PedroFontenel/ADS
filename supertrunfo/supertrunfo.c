2c942e53fbeb2996170b6a4a8ecd6124fd8700a2
Pedro Magno <pedrofontenelerocha@outlook.com>
2025-01-29 13:21:40 +0000

    registro de codigo

--- supertrunfo/supertrunfo.c
+++ supertrunfo/supertrunfo.c
@@ -10,38 +10,35 @@ int main(){
      
      Cada carta incluirá informações:
 
-     - Estado - Código da carta - Nome da cidade - População - PIB - Área - Quantidade de pontos turísticos - Densidade populacional - PIB per capita
+     País - Estado - Código da carta - Nome da cidade - População - PIB - Área - Quantidade de pontos turísticos - Densidade populacional - PIB per capita
     
-     */
-
-    char país [15];
+*/ 
     int codigo; // do 1 ao 4
 
-    printf("Qual o nome do país a ser atribuido para a carta? \n");
-    scanf("%s", &país);
-
     printf("Registre o codigo dessa carta: \n"); 
-    scanf("%d", &codigo);
-
-    printf("Parabens, carta cadastrada com sucesso \n Codígo da carta: %d\n", codigo);
-
+    scanf("%i", &codigo);
+    
     printf("Agora vamos cadastrar as informações dessa carta\n");
 
+    char pais [10];
+
+    printf("Qual o nome do país a ser atribuido para a carta? \n");
+    scanf("%s", &pais [10]);
+
     char estado, cidade [15];
-
-    printf("Diga um estado desse país: \n");
-    scanf("%s", &estado);
-
-    printf("Agora, uma cidade desse estado: \n");
-    scanf("%s", &cidade);
-
     int populacao, Pturistico;
     float area, PIB, PIBpercapita, densidadepopulacional;
+
+    printf("Diga um Estado desse país: \n");
+    scanf("%s", &estado);
+
+    printf("Agora, uma Cidade desse estado: \n");
+    scanf("%s", &cidade);
 
     printf("Agora gostaria que voçê me falasse a população geral dessa cidade: \n");
     scanf("%d", &populacao);
 
-    printf("Agoro, qual o numero de pontos turisticos dessa cidade: \n");
+    printf("Agora, qual o numero de pontos turisticos dessa cidade: \n");
     scanf("%d", &Pturistico);
 
     printf("Poderia me informar a area dessa cidade: \n");
@@ -56,16 +53,8 @@ int main(){
     printf("Por fim, gostaria que voçê me falasse a densidade populacional dessa cidade: \n");
     scanf("%d", &densidadepopulacional);
 
-    printf("Parabens, as informações dessa carta foram cadastradas com sucesso, seu codigo é: %i\n", codigo);
+    printf("Parabens, as informações da carta foram cadastradas com sucesso\n");
 
-    printf("País: %s\n", país);
-    printf("Estado: %s\n", estado);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Pontos turistícos: %d\n", Pturistico);
-    printf("PIB: %f\n", PIB);
-    printf("PIB per capita: %f\n", PIBpercapita);
-    printf("Densidade populacional: %d\n", densidadepopulacional);
-
+    
     return 0;
 }
